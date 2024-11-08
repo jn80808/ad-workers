b19f8da853ab4152821cad57d0ce8a6c6cbc7e37
JN <joyng808@gmail.com>
2024-11-09 01:10:42 +0800

    [JN]-Added Json File at Function Folder

--- script.js
+++ script.js
@@ -1,35 +1,13 @@
-function changeMessage() {
-    const greetingData = {
-        "Hello World!": [
-            { country: 1, name: "USA" },
-            { country: 2, name: "Philippines" },
-            { country: 3, name: "UK" }
-        ],
-        "¡Hola Mundo!": [
-            { country: 1, name: "Spain" },
-            { country: 2, name: "Mexico" },
-            { country: 3, name: "Argentina" }
-        ],
-        "Bonjour le Monde!": [
-            { country: 1, name: "France" },
-            { country: 2, name: "Canada" },
-            { country: 3, name: "Belgium" }
-        ],
-        "Hallo Welt!": [
-            { country: 1, name: "Germany" },
-            { country: 2, name: "Austria" },
-            { country: 3, name: "Switzerland" }
-        ],
-        "你好，世界！": [
-            { country: 1, name: "China" },
-            { country: 2, name: "Taiwan" },
-            { country: 3, name: "Singapore" }
-        ],
-        "こんにちは世界！": [
-            { country: 1, name: "Japan" }
-        ]
-    };
+let greetingData = {};
 
+fetch('functions/greetings.json')
+    .then(response => response.json())
+    .then(data => {
+        greetingData = data;
+    })
+    .catch(error => console.error('Error loading GreatingJSON:', error));
+
+function changeMessage() {
     const greetings = Object.keys(greetingData);
     const greeting = document.getElementById('greeting');
     const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
